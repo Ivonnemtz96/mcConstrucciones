a8eb80af90383fc19e0e00e1036bbeabe179f9a1
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-12-21 13:59:46 -0700

    info contacto ok

--- modules/contacto.php
+++ modules/contacto.php
@@ -4,8 +4,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-5 mt-45">
-                <h6>Get in touch</h6>
-                <h1>Contact <span>Us</span></h1>
+                <h6>¿Tienes alguna duda o busca cotización?</h6>
+                <h1>Contác<span>tanos</span></h1>
             </div>
         </div>
     </div>
@@ -15,71 +15,76 @@
     <div class="container">
         <div class="row mb-90">
             <div class="col-md-5 mb-60">
-                <h5>Contact Information</h5>
-                <p class="mb-30">Contact nullam usamcoen the drana duru metus utah osare asya mavna busnini viventa the
-                    ornare ipsum. Curabitur luctus mana numsation pellentesque the miss tenis mollie.</p>
+                <h5>Información de contacto</h5>
+                <p class="mb-30">
+                    Si tiene alguna duda o pregunta, estamos a sus órdenes.
+                </p>
                 <div class="contact-link">
                     <div class="contact-link-icon"><span class="norc-phone"></span></div>
                     <div class="contact-link-content">
-                        <div class="contact-link-title">Call us</div>
-                        <div class="contact-link-text">+1 203-333-4444</div>
+                        <div class="contact-link-title">Teléfono</div>
+                        <div class="contact-link-text"><span>Ing. Mario Mendoza</span> <a
+                                href="tel:6243583690">6243583690</a></div>
                     </div>
                 </div>
+
                 <div class="contact-link">
-                    <div class="contact-link-icon"><span class="fa fa-fax"></span></div>
+                    <div class="contact-link-icon"><span class="norc-phone"></span></div>
                     <div class="contact-link-content">
-                        <div class="contact-link-title">Fax Number</div>
-                        <div class="contact-link-text">+1 203-333-4400</div>
+                        <div class="contact-link-title">Teléfono</div>
+                        <div class="contact-link-text"><span>Ing. Raúl Mendoza</span> <a
+                                href="tel:6241450004">6241450004</a></div>
                     </div>
                 </div>
                 <div class="contact-link">
                     <div class="contact-link-icon"><span class="norc-mail"></span></div>
                     <div class="contact-link-content">
-                        <div class="contact-link-title">Send us an email</div>
-                        <div class="contact-link-text">info@construction.com</div>
+                        <div class="contact-link-title">Correo</div>
+                        <div class="contact-link-text"><a
+                                href="mailto:info@mcconstrucciones.com">info@mcconstrucciones.com</a></div>
                     </div>
                 </div>
                 <div class="contact-link">
                     <div class="contact-link-icon"><span class="norc-square-pin"></span></div>
                     <div class="contact-link-content">
-                        <div class="contact-link-title">Visit our office</div>
-                        <div class="contact-link-text">24 King St, Charleston, 29401 USA</div>
+                        <div class="contact-link-title">Dirección</div>
+                        <div class="contact-link-text">Local #2, calle Baja California E. 7 de Mayo y 15 de Septiembre,
+                            Col Zacatal</div>
                     </div>
                 </div>
             </div>
             <div class="col-md-5 offset-md-2">
                 <div class="form-wrap">
                     <div class="form-box">
-                        <h5>Get in touch</h5>
+                        <h5>Escríbenos</h5>
                         <form method="post" class="contact__form">
                             <!-- Form message -->
                             <div class="row">
                                 <div class="col-12">
                                     <div class="alert alert-success contact__msg" style="display: none" role="alert">
-                                        Your message was sent successfully. </div>
+                                        Su mensaje se ha enviado. </div>
                                 </div>
                             </div>
                             <!-- Form elements -->
                             <div class="row">
                                 <div class="col-md-12 form-group">
-                                    <input name="name" type="text" placeholder="Your Name *" required>
+                                    <input name="name" type="text" placeholder="Nombre *" required>
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <input name="email" type="email" placeholder="Your Email *" required>
+                                    <input name="email" type="email" placeholder="Correo *" required>
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <input name="phone" type="text" placeholder="Your Number *" required>
+                                    <input name="phone" type="text" placeholder="Teléfono *" required>
                                 </div>
                                 <div class="col-md-12 form-group">
-                                    <input name="subject" type="text" placeholder="Subject *" required>
+                                    <input name="subject" type="text" placeholder="Asunto *" required>
                                 </div>
                                 <div class="col-md-12 form-group">
-                                    <textarea name="message" id="message" cols="30" rows="4" placeholder="Message *"
+                                    <textarea name="message" id="message" cols="30" rows="4" placeholder="Mensaje *"
                                         required></textarea>
                                 </div>
                                 <div class="col-md-12">
-                                    <button class="button-secondary mt-15"><a href="#0"><span>Send
-                                                Message</span></a></button>
+                                    <button class="button-secondary mt-15"><a href="#0"><span>Enviar</span></a></button>
                                 </div>
                             </div>
                         </form>
@@ -91,8 +96,12 @@
         <div class="row">
             <div class="col-md-12 map-color animate-box" data-animate-effect="fadeInUp">
                 <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.0195304545014!2d-109.71400898479136!3d23.096380884915703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af5af80981a9fb%3A0xb9fa133ff13f11e5!2sMC%20CONSTRUCCIONES!5e0!3m2!1ses-419!2smx!4v1671655131457!5m2!1ses-419!2smx"
+                    width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <!-- <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1573147.7480448114!2d-74.84628175962355!3d41.04009641088412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25856139b3d33%3A0xb2739f33610a08ee!2s1616%20Broadway%2C%20New%20York%2C%20NY%2010019%2C%20Amerika%20Birle%C5%9Fik%20Devletleri!5e0!3m2!1str!2str!4v1646760525018!5m2!1str!2str"
-                    width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
             </div>
         </div>
     </div>
@@ -102,8 +111,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <p class="mb-0">Frequently Asked Questions</p>
-                <div class="section-title">Answer <span>Questions</span></div>
+                <p class="mb-0">Algunas preguntas de clientes</p>
+                <div class="section-title">Preguntas <span> Frecuentes</span></div>
             </div>
         </div>
         <div class="row">
@@ -112,7 +121,7 @@
                 <div class="accordion">
                     <div class="item">
                         <div class="title">
-                            <h6>What is the experience of your employees?</h6>
+                            <h6>Pregunta 1</h6>
                         </div>
                         <div class="accordion-info" style="display: none;">
                             <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
@@ -122,7 +131,7 @@
                     </div>
                     <div class="item">
                         <div class="title">
-                            <h6>What tools do you use in construction?</h6>
+                            <h6>Pregunta 2</h6>
                         </div>
                         <div class="accordion-info" style="display: none;">
                             <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
@@ -132,7 +141,7 @@
                     </div>
                     <div class="item">
                         <div class="title">
-                            <h6>Do you have free home delivery in the US?</h6>
+                            <h6>Pregunta 3</h6>
                         </div>
                         <div class="accordion-info" style="display: none;">
                             <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
@@ -142,7 +151,7 @@
                     </div>
                     <div class="item">
                         <div class="title">
-                            <h6>Can you help with the construction documents?</h6>
+                            <h6>Pregunta 4</h6>
                         </div>
                         <div class="accordion-info" style="display: none;">
                             <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
