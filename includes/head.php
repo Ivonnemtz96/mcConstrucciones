124dcf2a5ad9dc17f10cd46a952ba742329691a1
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-12-22 16:39:46 -0700

    cambio title

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>NORC. Construction</title>
+    <title>Mc Construcciones</title>
     <link rel="shortcut icon" href="/img/favicon.png" />
     <link rel="stylesheet" href="/css/plugins.css" />
     <link rel="stylesheet"
